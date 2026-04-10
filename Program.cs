f30deaa0caf89c1f4aba051be25519300b5b1108
Rizkirhmd8 <akimrhmd@gmail.com>
2026-04-10 15:25:01 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
-﻿int a = 10 ;
+#include <stdio.h>
+
+int a = 10;
 int b = 6;
 
-static int penjumlahan(int a, int b)
-{
+static int penjumlahan(int a, int b){
     return a + b;
 }
-
-static int pengurangan(int a, int b)
-{
+static int pengurangan(int a, int b){
     return a - b;
 }
-
-static int perkalian(int a, int b)
-{
+static int perkalian(int a, int b){
     return a * b;  
 }
-
-static int pembagian(int a, int b)
-{
+static int pembagian(int a, int b){
     return a / b;
+}
+static int modulus(int a, int b){
+    return a % b;
+}
+
+int main()
+{
+    printf("Hasil Penjumlahan: \n", penjumlahan(a, b));
+    printf("Hasil Pengurangan: \n", pengurangan(a, b));
+    printf("Hasil Perkalian: \n", perkalian(a, b));
+    printf("Hasil Pembagian: \n", pembagian(a, b));
+    printf("Hasil Modulus  : \n", modulus(a, b));
+
+    return 0;
 }
